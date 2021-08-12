ba7a88c6cb8f1409a3f8e4fa963fb4fc6b8ac6d9
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-12 15:32:07 +0530

    Update Group Anagrams.cpp
    https://leetcode.com/problems/group-anagrams/

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map <string, int > m;
-        vector<vector<string>> v(strs.size());
-        int ind=0;
-        for(string i: strs)
-        {
-            string s=i;
-            sort(s.begin(),s.end());
-            if(m.find(s)==m.end())  m[s]=ind++;
-            v[m[s]].push_back(i);
+        int n=strs.size();
+        map<string,vector<string>> m;
+        for(string s: strs) {
+            string d=s;
+            sort(d.begin(), d.end());
+            m[d].push_back(s);
         }
-        v.resize(m.size());
-        return v;
+        vector<vector<string>> ans;
+        for(auto [s, v]: m) {
+            ans.push_back(v);
+        }
+        return ans;
     }
 };
