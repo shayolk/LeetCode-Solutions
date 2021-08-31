f6def6ef3b067b706d818da766846755e43152de
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-31 17:56:25 +0530

    Update Best Time to Buy and Sell Stock IV.cpp
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/

--- Best Time to Buy and Sell Stock IV.cpp
+++ Best Time to Buy and Sell Stock IV.cpp
@@ -1,34 +1,25 @@
 class Solution {
+    const int INF=1e9+5;
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n=(int)prices.size();
-        if(k==0 || n<=1) return 0;
+    int maxProfit(int K, vector<int>& prices) {
+        int N=(int)prices.size();
+        if(N<=1 || K==0) return 0;
+        vector<vector<vector<int>>> dp(N+1, vector<vector<int>> (K+1, vector<int> (2, -INF)));
+        for(int i=0; i<=N; ++i) {
+            dp[i][0][1]=0;
+        }
+        for(int j=0; j<=K; ++j) {
+            dp[0][j][1]=0;
+        }
+        for(int i=1; i<=N; ++i) {
+            for(int j=1; j<=K; ++j) {
+                dp[i][j][1]=max(dp[i-1][j][1], dp[i-1][j][0]+prices[i-1]);
+                dp[i][j][0]=max(dp[i-1][j][0], dp[i-1][j-1][1]-prices[i-1]);
+            }
+        }
         int ans=0;
-        if(2*k>=n) {
-            for(int i=1; i<n; ++i) {
-                ans+=max(0, prices[i]-prices[i-1]);
-            }
-            return ans;
-        }
-        int dp[n][k+1][2];
-        for(int i=0; i<n; ++i) {
-            for(int j=0; j<=k; ++j) {
-                dp[i][j][0]=-2e9;
-                dp[i][j][1]=-2e9;
-            }
-        }
-        for(int i=0; i<n; ++i) {
-            dp[i][0][0]=0;
-        }
-        dp[0][1][1]=-prices[0];
-        for(int i=1; i<n; ++i) {
-            for(int j=1; j<=k; ++j) {
-                dp[i][j][0]=max(dp[i-1][j][0], dp[i-1][j][1]+prices[i]);
-                dp[i][j][1]=max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i]);
-            }
-        }
-        for(int j=0; j<=k; ++j) {
-            ans=max(ans, dp[n-1][j][0]);
+        for(int j=0; j<=K; ++j) {
+            ans=max(ans, dp[N][j][1]);
         }
         return ans;
     }
