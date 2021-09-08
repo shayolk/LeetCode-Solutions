397e50749a8394bee8d75c2394e9de4261ca2756
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-08 16:39:09 +0530

    Create Shifting Letters.cpp
    https://leetcode.com/problems/shifting-letters/

---
+++ Shifting Letters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n=s.size();
+        shifts[n-1]%=26;
+        for(int i=n-2; i>=0; --i) {
+            shifts[i]+=shifts[i+1];
+            shifts[i]%=26;
+        }
+        for(int i=0; i<n; ++i) {
+            s[i]='a'+((s[i]-'a')+shifts[i])%26;
+        }
+        return s;
+    }
+};
