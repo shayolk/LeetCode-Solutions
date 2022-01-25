47f82444d40694e86d82cba582815d0f64494724
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-01-25 16:55:58 +0530

    Update Combination Sum.cpp
    https://leetcode.com/problems/combination-sum/

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
     vector<vector<int>> ans;
     
-    void find(int beg, int sum, int b, vector<int> now, vector<int> &a) {
-        if(sum>b) return;
-        if(sum==b) {
+    void find(int i, int n, int target, vector<int>& now, vector<int>& a) {
+        if(target==0) {
             ans.push_back(now);
             return;
         }
-        for(int i=beg; i<a.size(); ++i) {
-            now.push_back(a[i]);
-            find(i, sum+a[i], b, now, a);
-            now.pop_back();
+        if(i==n || target<0) {
+            return;
         }
+        find(i+1, n, target, now, a);
+        now.push_back(a[i]);
+        find(i, n, target-a[i], now, a);
+        now.pop_back();
     }
     
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        find(0, 0, target, vector<int>{}, candidates);
+        vector<int> now;
+        find(0, (int)candidates.size(), target, now, candidates);
         return ans;
     }
 };
