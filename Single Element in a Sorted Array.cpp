9b98a3c0cc7f769787c0238dc210ba587b274568
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-02-20 19:56:27 +0530

    Update Single Element in a Sorted Array.cpp
    https://leetcode.com/problems/single-element-in-a-sorted-array/

--- Single Element in a Sorted Array.cpp
+++ Single Element in a Sorted Array.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int x = 0;
-        for(int i: nums) {
-            x ^= i;
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-2]!=nums[n-1]) return nums[n-1];
+        int lo=1, hi=n-2, mid;
+        while(lo<=hi) {
+            mid=lo+(hi-lo)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            int l=mid+(nums[mid]==nums[mid-1]);
+            if(l%2) hi=mid-1;
+            else lo=mid+1;
         }
-        return x;
+        return -1;
     }
 };
