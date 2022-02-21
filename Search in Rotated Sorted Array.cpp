642407765f23aaf0c3b880761685c790a8abb143
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-02-21 11:55:32 +0530

    Update Search in Rotated Sorted Array.cpp
    https://leetcode.com/problems/search-in-rotated-sorted-array/

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size(), lo=0, hi=nums.size()-1, mid;
-        mid=lo+(hi-lo)/2;
-        while(lo<=hi)
-        {
-            if(target==nums[mid])
-                return mid;
-            else if(target==nums[lo])
-                return lo;
-            else if(target==nums[hi])
-                return hi;
-            else if(nums[mid]>nums[lo]) {
-                if(target<nums[mid]) {
-                    if(target>nums[lo])
-                        hi=mid-1;
-                    else
-                        lo=mid+1;
+        int n=nums.size();
+        int lo=0, hi=n-1;
+        while(lo<=hi) {
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            if(target<nums[mid]) {
+                if(nums[0]<=nums[mid]) {
+                    if(nums[lo]<=target && target<nums[mid]) hi=mid-1;
+                    else lo=mid+1;
                 }
-                else 
-                    lo=mid+1;
+                else {
+                    if(nums[mid]<target && target<=nums[hi]) lo=mid+1;
+                    else hi=mid-1;
+                }
             }
             else {
-                if(target>nums[mid]) {
-                    if(target<nums[hi])
-                        lo=mid+1;
-                    else
-                        hi=mid-1;
+                if(nums[mid]<=nums[n-1]) {
+                    if(nums[mid]<target && target<=nums[hi]) lo=mid+1;
+                    else hi=mid-1;
                 }
-                else
-                    hi=mid-1;
+                else {
+                    if(nums[lo]<=target && target<nums[mid]) hi=mid-1;
+                    else lo=mid+1;
+                }
             }
-            mid=lo+(hi-lo)/2;
         }
         return -1;
     }
