8ed5a55e6c251bcb4b06286382d8619dbafe096a
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-02-24 20:35:07 +0530

    Create Next Greater Element II.cpp
    https://leetcode.com/problems/next-greater-element-ii/

---
+++ Next Greater Element II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n, -1);
+        stack<int> s;
+        for(int i=2*n-1; i>=0; --i) {
+            while(!s.empty() && s.top()<=nums[i%n]) {
+                s.pop();
+            }
+            if(i<n && !s.empty()) {
+                ans[i%n]=s.top();
+            }
+            s.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
