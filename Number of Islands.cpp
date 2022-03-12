81f0fa6a793604654526ceaf7faf0c279e51f0e7
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-12 17:41:23 +0530

    Update Number of Islands.cpp
    https://leetcode.com/problems/number-of-islands/

--- Number of Islands.cpp
+++ Number of Islands.cpp
@@ -1,43 +1,36 @@
 class Solution {
-    int a[4]={0, 1, 0, -1};
-    int b[4]={1, 0, -1, 0};
-    void trav(int x, int y, int n, int m, vector <vector <char>> grid, vector <vector <int>>& vis) {
-        queue <pair <int,int>> q;
-        q.push({x,y});
-        vis[x][y]=1;
-        while(!q.empty()) {
-            pair<int,int> p=q.front();
-            q.pop();
-            for(int i=0; i<4; ++i) {
-                if(p.first+a[i]>=0 && p.first+a[i]<n && p.second+b[i]>=0 && p.second+b[i]<m && grid[p.first+a[i]][p.second+b[i]]=='1' && !vis[p.first+a[i]][p.second+b[i]]) {
-                    vis[p.first+a[i]][p.second+b[i]]=1;
-                    q.push({p.first+a[i], p.second+b[i]});
-                }
+    int n, m, ans;
+    vector<vector<bool>> vis;
+    
+    const int dr[4]={0, 0, 1, -1};
+    const int dc[4]={1, -1, 0, 0};
+    
+    bool safe(int r, int c) {
+        return r>=0 && r<n && c>=0 && c<m;
+    }
+    
+    void dfs(int r, int c, vector<vector<char>>& grid) {
+        vis[r][c]=true;
+        for(int d=0; d<4; ++d) {
+            int rr=r+dr[d], cc=c+dc[d];
+            if(safe(rr, cc) && !vis[rr][cc] && grid[rr][cc]=='1') {
+                dfs(rr, cc, grid);
             }
         }
     }
+    
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()) {
-            return 0;
-        }
-        int n=grid.size(), m=grid[0].size();
-        vector <vector <int>> vis(n, vector<int> (m, 0));
-        vector <pair <int,int>> p;
-        for(int i=0; i<n; ++i)
+        n=grid.size();
+        m=grid[0].size();
+        ans=0;
+        vis.resize(n, vector<bool> (m));
+        for(int i=0; i<n; ++i) {
             for(int j=0; j<m; ++j) {
-                if(grid[i][j]=='1') {
-                    p.push_back({i,j});
+                if(!vis[i][j] && grid[i][j]=='1') {
+                    ++ans;
+                    dfs(i, j, grid);
                 }
-            }
-        if(p.empty()) {
-            return 0;
-        }
-        int ans=0;
-        for(auto i: p) {
-            if(!vis[i.first][i.second]) {
-                ++ans;
-                trav(i.first,i.second,n,m,grid,vis);
             }
         }
         return ans;
