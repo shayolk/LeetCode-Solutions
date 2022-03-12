79cfc0c05b3c7fb1ebddccfa22d92c4fc76174b4
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-12 19:17:39 +0530

    Update Two Sum.cpp
    https://leetcode.com/problems/two-sum/

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> ind;
-        for(int i=0; i<nums.size(); ++i) {
-            if(ind.count(nums[i])) {
-                return vector<int>{ind[nums[i]], i};
-            }
-            ind[target-nums[i]]=i;
+        int n=nums.size();
+        vector<pair<int,int>> a;
+        for(int i=0; i<n; ++i) {
+            a.push_back({nums[i], i});
         }
-        return vector<int>{-1, -1};
+        sort(a.begin(), a.end());
+        int l=0, r=n-1;
+        while(l<r) {
+            if(a[l].first+a[r].first<target) ++l;
+            else if(a[l].first+a[r].first>target) --r;
+            else break;
+        }
+        return {a[l].second, a[r].second};
     }
 };
