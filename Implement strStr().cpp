8bd24253f3f91eb651568e52c7a239dce95dd915
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-15 20:43:47 +0530

    Update Implement strStr().cpp
    https://leetcode.com/problems/implement-strstr/

--- Implement strStr().cpp
+++ Implement strStr().cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=haystack.size(), m=needle.size();
-        if(!m) return 0;
-        vector<int> lps(m);
-        for(int i=1; i<m; ++i) {
+        if(needle.empty()) return 0;
+        int m=needle.size();
+        needle+="#"+haystack;
+        int n=needle.size();
+        vector<int> lps(n);
+        for(int i=1; i<n; ++i) {
             int j=lps[i-1];
             while(j && needle[i]!=needle[j]) {
                 j=lps[j-1];
             }
             if(needle[i]==needle[j]) ++j;
             lps[i]=j;
-        }
-        int i=0, j=0;
-        while(i<n) {
-            if(haystack[i]==needle[j]) {
-                ++i;
-                ++j;
-            }
-            else {
-                if(j) j=lps[j-1];
-                else ++i;
-            }
-            if(j==m) return i-m;
+            if(lps[i]==m) return i-2*m;
         }
         return -1;
     }
