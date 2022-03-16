72132f73afaf63d791ba4781a2e579da6dc3c449
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-16 16:01:10 +0530

    Create Binary Tree Preorder Traversal.cpp
    https://leetcode.com/problems/binary-tree-preorder-traversal/

---
+++ Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int> ans;
+    
+    void visit(TreeNode* root) {
+        if(!root) return;
+        ans.push_back(root->val);
+        visit(root->left);
+        visit(root->right);
+    }
+    
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        visit(root);
+        return ans;
+    }
+};
