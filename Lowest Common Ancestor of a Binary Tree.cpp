8cc282ad3a3a93f9d6d8e85e86fe67b27ef26253
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-18 11:25:11 +0530

    Update Lowest Common Ancestor of a Binary Tree.cpp
    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/

--- Lowest Common Ancestor of a Binary Tree.cpp
+++ Lowest Common Ancestor of a Binary Tree.cpp
@@ -8,47 +8,13 @@
  * };
  */
 class Solution {
-    vector<int> depth;
-    vector<TreeNode*> node;
-    map<TreeNode*, int> last;
-    int tot=0;
-    
-    void dfs(TreeNode* root, int height) {
-        if(!root) {
-            return;
-        }
-        node.push_back(root);
-        depth.push_back(height);
-        last[root]=tot++;
-        
-        dfs(root->left, height+1);
-        
-        node.push_back(root);
-        depth.push_back(height);
-        last[root]=tot++;
-        
-        dfs(root->right, height+1);
-        
-        node.push_back(root);
-        depth.push_back(height);
-        last[root]=tot++;
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root || !p || !q) {
-            return NULL;
-        }
-        dfs(root, 0);
-        // for(int i=0; i<depth.size(); ++i) cout<<node[i]->val<<" "<<depth[i]<<"\n";
-        int lastp=last[p], lastq=last[q];
-        int st=min(lastp, lastq), ed=max(lastp, lastq);
-        int ind=-1, mn=2e9+5;
-        for(int i=st; i<=ed; ++i) {
-            if(depth[i]<mn) {
-                mn=depth[i];
-                ind=i;
-            }
-        }
-        return node[ind];
+        if(!root || root==p || root==q) return root;
+        TreeNode* l=lowestCommonAncestor(root->left, p, q);
+        TreeNode* r=lowestCommonAncestor(root->right, p, q);
+        if(!l) return r;
+        if(!r) return l;
+        return root;
     }
 };
