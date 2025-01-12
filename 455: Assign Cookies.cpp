a66f1d6533df35ac7323effad0a32914705473c5
Shayak <44469461+shayolk@users.noreply.github.com>
2025-01-12 23:09:51 +0000

    https://leetcode.com/problems/assign-cookies/

---
+++ 455: Assign Cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int n = g.size(), m = s.size();
+        int i = 0, j = 0, ans = 0;
+        while(i<n && j<m) {
+            if(g[i] <= s[j]) {
+                ++i;
+                ++ans;
+            }
+            ++j;
+        }
+        return ans;
+    }
+};
