f348b106b4c3a1e9834d1276b57d6465e864f48e
Shayak <44469461+shayolk@users.noreply.github.com>
2026-01-31 23:45:05 +0000

    https://leetcode.com/problems/sort-colors/
    https://leetcode.com/problems/sort-colors/

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int zero = 0, one = 0, two = n-1;
+        while (one <= two) {
+            switch (nums[one]) {
+                case 0: {
+                    swap(nums[one++], nums[zero++]);
+                    break;
+                }
+                case 1: {
+                    ++one;
+                    break;
+                }
+                case 2: {
+                    swap(nums[one], nums[two--]);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+    }
+};
