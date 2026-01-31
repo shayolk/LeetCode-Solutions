a17b6e230f03896b06ea59ee3fca9c84d2eba60c
Shayak <44469461+shayolk@users.noreply.github.com>
2026-01-31 21:28:54 +0000

    https://leetcode.com/problems/next-permutation/description/
    https://leetcode.com/problems/next-permutation/description/

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-1;
+        while (i && nums[i-1] >= nums[i]) {
+            --i;
+        }
+        if (i) {
+            for(int j = n-1; j >= i; --j) {
+                if(nums[j] > nums[i-1]) {
+                    swap(nums[i-1], nums[j]);
+                    break;
+                }
+            }
+        }
+        reverse(nums.begin() + i, nums.end());
+    }
+};
